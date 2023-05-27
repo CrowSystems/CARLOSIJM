eb6fd7541c7bb6c05ca0729a815a1b5b2eabe7ef
CARLOSIJM <carlosidjmdz@gmail.com>
2023-05-26 23:31:40 -0600

    Carga de scripts

--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@ let sections = document.querySelectorAll('section');
 let section = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
-    section.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
-        let id = sec.getAtrribute('id');
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            //active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
     });
     //sticky header
     let header = document.querySelector('header');
